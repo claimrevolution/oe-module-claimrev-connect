a7b25b7c070df20a4f767d1a5a469ac9e699cd92
Brad <brad.sharp@claimrev.com>
2022-12-26 08:29:39 -0600

    sql script was using a wrong name

--- table.sql
+++ table.sql
@@ -25,7 +25,7 @@ INSERT INTO `background_services` (`name`, `title`, `active`, `running`, `next_r
 ('ClaimRev_Receive', 'Get Reports from ClaimRev', 1, 0, '2017-05-09 17:39:10', 240, 'start_X12_Claimrev_get_reports', '/interface/modules/custom_modules/oe-module-claimrev-connect/src/billing_claimrev_service.php', 100);
 #Endif
 
-#IfNotRow background_services name ClaimRev_Receive
+#IfNotRow background_services name ClaimRev_Elig_Send_Receive
 INSERT INTO `background_services` (`name`, `title`, `active`, `running`, `next_run`, `execute_interval`, `function`, `require_once`, `sort_order`) VALUES
 ('ClaimRev_Elig_Send_Receive', 'Send and Receive Eligibility from ClaimRev', 1, 0, '2017-05-09 17:39:10', 1, 'start_send_eligibility', '/interface/modules/custom_modules/oe-module-claimrev-connect/src/Eligibility_ClaimRev_Service.php', 100);
 #Endif
