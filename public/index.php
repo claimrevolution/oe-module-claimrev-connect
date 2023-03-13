7b80696bf031586602e046f113d27d9c49f056e9
Brad <brad.sharp@claimrev.com>
2023-03-13 10:20:52 -0500

    updates to index.php

--- public/index.php
+++ public/index.php
@@ -22,7 +22,26 @@
     <div class="row"> 
         <div class="col">
             <div class="card">
-                <p>Welcome to the ClaimRev Connector. This is your link to the portal and claim processing. You can view claims sent to ClaimRev and manually send and receive files.</p>
+                <p>
+                    Welcome to the ClaimRev Connector. This is your link to the portal and claim processing.
+
+                </p>
+                <h6>Tab Descriptions</h6>
+                <ul>
+                    <li>
+                        Claims -> Lets you search claims sent to ClaimRev and view the status.
+                    </li>
+                    <li>
+                        X12 Tracker -> Display's files that have been submitted or in the process of being submitted.
+                    </li>
+                    <li>
+                        Setup -> Helps identify any setup issues along with checking background services.
+                    </li>
+                    <li>
+                        Connectivity -> Display's information that may help support fix connection issues you maybe having.
+                    </li>
+                </ul>
+
                 <h6>Support/Sales</h6>
                 <ul>
                     <li> Call: <a href="tel:9189430020">1-918-943-0020<a>   </li>
